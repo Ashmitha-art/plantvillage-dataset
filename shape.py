8a0c03d02d15f16047b185ded164cca9911c73ce
sihambyte <sihamargaw@gmail.com>
2024-03-13 21:59:39 -0700

    implemented KNN and descion tree algo

--- shape.py
+++ shape.py
@@ -2,6 +2,11 @@ import os
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt 
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
+
 
 def extractShape(image):
     #calculate Moments
@@ -17,6 +22,9 @@ def extractShape(image):
 
 grayscale_folder = 'grayscale'
 
+features = []
+labels = []
+
 # Iterate only through the grayscale folder
 for subfolder in os.listdir(grayscale_folder):
     folder_path = os.path.join(grayscale_folder, subfolder)
@@ -31,4 +39,36 @@ for subfolder in os.listdir(grayscale_folder):
                 labels.append(subfolder)  # Using the subfolder name as the label
             else:
                 print("Failed to load image:", image_path)
-                    
+
+features = np.array(features)
+labels = np.array(labels)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+# Initialize the decision tree classifier
+DTC = DecisionTreeClassifier()
+
+# Fit the model to the training data
+DTC.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = DTC.predict(X_test)
+
+# Calculate and print the accuracy of the model
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy of descion tree model: {accuracy}")
+
+knn = KNeighborsClassifier(n_neighbors=5)
+
+# Fit the model to the training data
+knn.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = knn.predict(X_test)
+
+# Calculate and print the accuracy of the model
+accuracy1 = accuracy_score(y_test, y_pred)
+print(f"Accuracy of knn model: {accuracy1}")
+
+
